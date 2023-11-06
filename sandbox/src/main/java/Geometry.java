bc28f0537a0aa62e3b7bc9d2ef967d6f7cd2fff7
Malki <magdamalk@gmail.com>
2023-11-06 16:15:18 +0700

    Промежуточный результат

---
+++ sandbox/src/main/java/Geometry.java
@@ -0,0 +1,8 @@
+public class Geometry {
+    public static void main(String[] args) {
+        printSquareArea(7);
+        printSquareArea(3.5);
+    }
+    static void printSquareArea(double a) {
+        System.out.println("Площадь квадрата со стороной " + a + " = " + (a*a));}
+}
