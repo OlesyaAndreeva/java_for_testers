74a4ed5738c074d02fb2e78fad333dabf2618687
Malki <magdamalk@gmail.com>
2023-11-07 22:06:38 +0700

    Квадрат+прямоугольник

--- sandbox/src/main/java/Geometry.java
+++ sandbox/src/main/java/Geometry.java
@@ -2,7 +2,24 @@ public class Geometry {
     public static void main(String[] args) {
         printSquareArea(7);
         printSquareArea(3.5);
+
+        printRectangleArea(3.0, 5.0);
     }
-    static void printSquareArea(double a) {
-        System.out.println("Площадь квадрата со стороной " + a + " = " + (a*a));}
+
+    private static void printRectangleArea(double a, double b) {
+        System.out.println("Площадь прямоугольника со сторонами "+ a + " и " + b + " = " + rectangleArea (a, b));
+    }
+
+    private static double rectangleArea(double a, double b) {
+        return a*b;
+    }
+
+    static void printSquareArea(double side) {
+        System.out.println("Площадь квадрата со стороной " + side + " = " + squareArea(side));
+    }
+
+    private static double squareArea(double a) {
+        return a * a;
+    }
+
 }
